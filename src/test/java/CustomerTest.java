6190faf9d08bb04abea00cce495587ec26dc2f2c
nhat hoang <hoangnh092185@gmail.com>
2016-10-03 11:11:31 -0700

    create customer methods

---
+++ src/test/java/CustomerTest.java
@@ -0,0 +1,85 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.text.DateFormat;
+
+
+public class CustomerTest {
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void customer_instantiatesCorrectly_true() {
+    Customer testCustomer = new Customer("Henry", "503-123-4567");
+    assertEquals(true, testCustomer instanceof Customer);
+  }
+  @Test
+  public void getName_customerInstantiatesWithName_Henry() {
+  Customer testCustomer = new Customer("Henry", "503-123-4567");
+  assertEquals("Henry", testCustomer.getName());
+  }
+  @Test
+  public void getPhone_customerInstantiatesWithPhone_String() {
+  Customer testCustomer = new Customer("Henry", "503-123-4567");
+  assertEquals( "503-123-4567", testCustomer.getPhone());
+  }
+  @Test
+  public void equals_returnsTrueIfNameAndPhoneAreSame_true() {
+    Customer firstCustomer = new Customer("Henry", "503-123-4567");
+    Customer anotherCustomer = new Customer("Henry", "503-123-4567");
+    assertTrue(firstCustomer.equals(anotherCustomer));
+  }
+  @Test
+  public void save_insertsObjectIntoDatabase_Customer() {
+    Customer testCustomer = new Customer("Henry", "503-123-4567");
+    testCustomer.save();
+    assertTrue(Customer.all().get(0).equals(testCustomer));
+  }
+  @Test
+  public void all_returnsAllInstancesOfCustomer_true() {
+    Customer firstCustomer = new Customer("Henry", "503-123-4567");
+    firstCustomer.save();
+    Customer secondCustomer = new Customer("Harriet", "503-321-7654");
+    secondCustomer.save();
+    assertEquals(true, Customer.all().get(0).equals(firstCustomer));
+    assertEquals(true, Customer.all().get(1).equals(secondCustomer));
+  }
+  @Test
+   public void save_assignsIdToObject() {
+     Customer testCustomer = new Customer("Henry", "503-123-4567");
+     testCustomer.save();
+     Customer savedCustomer = Customer.all().get(0);
+     assertEquals(testCustomer.getId(), savedCustomer.getId());
+   }
+   @Test
+    public void find_returnsCustomerWithSameId_secondCustomer() {
+      Customer firstCustomer = new Customer("Henry", "503-123-4567");
+      firstCustomer.save();
+      Customer secondCustomer = new Customer("Harriet", "503-321-7654");
+      secondCustomer.save();
+      assertEquals(Customer.find(secondCustomer.getId()), secondCustomer);
+    }
+    @Test
+    public void saveOrderTime_recordsOrderTimeOfCreationInDatabase() {
+     Customer testCustomer = new Customer("Henry", "503-123-4567");
+     testCustomer.save();
+     Timestamp savedCustomerOrderTime = Customer.find(testCustomer.getId()).getOrderTime();
+     Timestamp rightNow = new Timestamp(new Date().getTime());
+     assertEquals(rightNow.getDay(), savedCustomerOrderTime.getDay());
+    }
+    // @Test
+    // public void getCustomers_retrievesAllCustomersFromDatabase_monstersList() {
+    //   Customer testCustomer = new Customer("Henry", "503-123-4567");
+    //   testCustomer.save();
+    //   FireCustomer firstCustomer = new FireCustomer("Smokey", testCustomer.getId());
+    //   firstCustomer.save();
+    //   WaterCustomer secondCustomer = new WaterCustomer("Drippy", testCustomer.getId());
+    //   secondCustomer.save();
+    //   Object[] monsters = new Object[] {firstCustomer, secondCustomer };
+    //   assertTrue(testCustomer.getCustomers().containsAll(Arrays.asList(monsters)));
+    // }
+
+}
---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,18 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+  @Override
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/delivery_service_test", null, null);
+  }
+
+  @Override
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteCustomersQuery = "DELETE FROM customers *;";
+      con.createQuery(deleteCustomersQuery).executeUpdate();
+    }
+  }
+}
