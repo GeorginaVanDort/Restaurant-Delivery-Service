5a30a6f1d542903c53d7f605ae1e5d121116c5aa
georgina van dort <georginavandort@gmail.com>
2016-10-04 15:59:07 -0700

    assert menu item class is passing all tests

--- src/test/java/MenuItemTest.java
+++ src/test/java/MenuItemTest.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.ArrayList;
 import java.math.*;
 
-
-
 public class MenuItemTest {
 
   @Rule
@@ -36,26 +34,39 @@ public class MenuItemTest {
      }
 
   @Test
-   public void all_returnsAllInstancesOfMenuItem_true() {
-     MenuItem firstMenuItem = menuitem;
-     firstMenuItem.save();
-     MenuItem secondMenuItem = new MenuItem("Burger", 2, 12.50);
-     secondMenuItem.save();
-     assertEquals(true, MenuItem.all().get(0).equals(firstMenuItem));
-     assertEquals(true, MenuItem.all().get(1).equals(secondMenuItem));
+    public void all_returnsAllInstancesOfMenuItem_true() {
+       MenuItem firstMenuItem = menuitem;
+       firstMenuItem.save();
+       MenuItem secondMenuItem = new MenuItem("Burger", 2, 12.50);
+       secondMenuItem.save();
+       assertEquals(true, MenuItem.all().get(0).equals(firstMenuItem));
+       assertEquals(true, MenuItem.all().get(1).equals(secondMenuItem));
+   }
+
+  @Test
+    public void save_assignsIdToObject_True() {
+       MenuItem testMenuItem = menuitem;
+       testMenuItem.save();
+       MenuItem savedMenuItem = MenuItem.all().get(0);
+       assertEquals(testMenuItem.getId(), savedMenuItem.getId());
+   }
+
+  @Test
+   public void find_returnsMenuItemWithSameId_secondMenuItem() {
+       MenuItem firstMenuItem = menuitem;
+       firstMenuItem.save();
+       MenuItem secondMenuItem = menuitem;
+       secondMenuItem.save();
+       assertEquals(MenuItem.find(secondMenuItem.getId()), secondMenuItem);
    }
 
    @Test
-   public void save_assignsIdToObject_True() {
-     MenuItem testMenuItem = menuitem;
-     testMenuItem.save();
-     MenuItem savedMenuItem = MenuItem.all().get(0);
-     assertEquals(testMenuItem.getId(), savedMenuItem.getId());
+   public void delete_deletesMenuItem_true(){
+     MenuItem firstMenuItem = menuitem;
+     firstMenuItem.save();
+     firstMenuItem.delete();
+     assertEquals(null, Restaurant.find(firstMenuItem.getId()));
    }
-
-
-
-
 
 
 
