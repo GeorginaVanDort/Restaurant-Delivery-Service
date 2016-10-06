d903511cd9ce4b86a66284e220cd57a3e1805bc7
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-10-06 16:09:54 -0700

    add method to app.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -28,10 +28,9 @@ public class App {
 
     get("/orderform", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      Restaurant newRestaurant = new Restaurant("happyHouse", "Chinese", "Late Night", "ChinaTown", "$$", 2);
-      newRestaurant.save();
+
       List<MenuItem> restaurantMenu = MenuItem.findByRestaurant(2);
-      model.put("restaurant", newRestaurant);
+      model.put("restaurant", restaurantMenu);
       model.put("menuitems", MenuItem.all());
       model.put("template", "templates/Orderforms.vtl");
       return new ModelAndView(model, layout);
